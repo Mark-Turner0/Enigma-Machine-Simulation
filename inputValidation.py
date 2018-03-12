5c40c21c67333eb61c64d6c8f66f11f37caa1e83
Mark Turner <14060149+MarkTurner01@users.noreply.github.com>
2018-03-12 13:32:10 +0000

    Update inputValidation.py

--- inputValidation.py
+++ inputValidation.py
@@ -1,14 +1,19 @@
 def validGearSelection(selected):
 	if int(selected) > 0 and int(selected) < 6:
 		return True
-		selected = 99
-	else:
-		return False
+	else: return False
+
+def validOrientationInput(selected):
+	if int(selected) > 0 and int(selected) < 27:
+		return True
+	else: return False
+
+def plugboardnumberValidation(selected):
+	if int(selected) > -1 and int(selected) < 14:
+		return True
+	else: return False
 
 def plugboardValidation(settings, selected):
 	if selected in settings:
 		return False
-		print("You have already used this character.")
-	else:
-		return True 
-
+	else: return True 
